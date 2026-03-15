f10ad5740388aacbc6026fda1dd0e919ef7ce222
Nattyy24 <malifeaye2485@gmail.com>
2026-03-15 22:50:39 +0300

    Import Auth component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { supabase } from "../supabaseClient";
-
+import Auth from "./components/Auth";
 export default function Auth() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
