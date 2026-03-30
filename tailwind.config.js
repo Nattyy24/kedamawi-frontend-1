0ca97d58baf7118af81c025e708dea57933e1f93
Natty24 <malifeaye2485@gmail.com>
2026-03-30 23:49:29 +0300

    fix tailwind color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {
       colors: {
         primary: "#121212",
         gold: "#D4AF37",
-        goldSoft: "#BF953F",
-        goldLight: "#FCF6BA",
-      }
+      },
     },
   },
   plugins: [],
